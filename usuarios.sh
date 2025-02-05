5cebdb47cda9861dedb129e2135c0caa15816721
robincajas12 <robincajas2020@gmail.com>
2025-02-04 21:17:51 -0500

    a

---
+++ usuarios.sh
@@ -0,0 +1,5 @@
+PG_HOST="localhost"          # Dirección de tu servidor PostgreSQL
+PG_USER="postgres"         # Tu nombre de usuario de PostgreSQL
+PG_PORT="5432"               # Puerto de PostgreSQL (por defecto es 5432)
+SQL_DIR="./usuarios"
+ psql -h $PG_HOST -U $PG_USER -p $PG_PORT -f $SQL_DIR/usuarios.sql
